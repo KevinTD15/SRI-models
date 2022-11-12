8d8f481e4f6bf87f8a7b4d5a59df653f2bb51f3b
KevinTD15 <talaveradiazkevin1999@gmail.com>
2022-11-12 06:49:53 -0500

    listo para ejecutar

--- Modelo booleano/main.py
+++ Modelo booleano/main.py
@@ -1,11 +1,17 @@
 from os import listdir
+from model import *
 
 def main():
     print('INGRESE EL PATH DONDE DESEA REALIZAR LA BUSQUEDA')
     path = input()
+    print('INGRESE LA CONSULTA DESEADA')
+    query = input()
+    
     content = listdir(path)
+    
+    result = ExcecuteModel(content, query)
 
-    print(content)
-
+    print(result)
+    
 if __name__ == '__main__':
     main()
