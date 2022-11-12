075b85bb0c4170a5acc023dc6e819c05b7cb34e6
KevinTD15 <talaveradiazkevin1999@gmail.com>
2022-11-12 17:08:05 -0500

    listo para recibir cualquier consulta

--- Modelo booleano/main.py
+++ Modelo booleano/main.py
@@ -2,16 +2,36 @@ from os import listdir
 from model import *
 
 def main():
-    print('INGRESE EL PATH DONDE DESEA REALIZAR LA BUSQUEDA')
-    path = input()
-    print('INGRESE LA CONSULTA DESEADA')
-    query = input()
-    
-    content = listdir(path)
-    
-    result = ExcecuteModel(content, query)
+    fin = ''
+    while(fin != '1'):
+        print('INGRESE EL PATH DONDE DESEA REALIZAR LA BUSQUEDA')
+        path = input()
+        print('MODO DE CONSULTA. TECLEE 1 O 2:')
+        print('1 - Casual')
+        print('2 - Experto')
+        queryMode = input()
+        print('TIPO DE COINCIDENCIA. TECLEE 1 O 2:')
+        print('1 - TOTAL') #AND
+        print('2 - PARCIAL') #OR
+        coincidence = input()
+        print('INGRESE LA CONSULTA DESEADA')
+        query = input()
 
-    print(result)
+        content = listdir(path)
+
+        result = ExcecuteModel(content, query, queryMode, coincidence)
+
+        print('\nRESULTADOS DE LA CONSULTA: \n')
+        if(len(result) == 0):
+            print('NO SE ENCONTRARON COINCIDENCIAS')
+        for i, doc in enumerate(result):
+            print(i, '-', doc)
+        print('\n')
+        
+        print('TECLEE: ')
+        print('1 - Salir')
+        print('2 - Hacer otra consulta')
+        fin = input()
     
 if __name__ == '__main__':
     main()
