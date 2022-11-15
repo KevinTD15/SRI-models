e1647bc34fb6b7cb0ad2369e0233230422faae49
KevinTD15 <talaveradiazkevin1999@gmail.com>
2022-11-15 00:42:08 -0500

    acomodado para los textos de los docs

--- Modelo booleano/main.py
+++ Modelo booleano/main.py
@@ -1,33 +1,43 @@
 from os import listdir
 from model import *
+import os
+from files import LoadFile
 
 def main():
     fin = ''
     while(fin != '1'):
+        flag = True
         print('INGRESE EL PATH DONDE DESEA REALIZAR LA BUSQUEDA')
         path = input()
-        print('MODO DE CONSULTA. TECLEE 1 O 2:')
-        print('1 - Casual')
-        print('2 - Experto')
-        queryMode = input()
-        print('TIPO DE COINCIDENCIA. TECLEE 1 O 2:')
-        print('1 - TOTAL') #AND
-        print('2 - PARCIAL') #OR
-        coincidence = input()
-        print('INGRESE LA CONSULTA DESEADA')
-        query = input()
+        if os.path.exists(path):
+            print('MODO DE CONSULTA. TECLEE 1 O 2:')
+            print('1 - Casual')
+            print('2 - Experto')
+            queryMode = input()
+            
+            coincidence = '0'
+            if(queryMode == '1'):
+                print('TIPO DE COINCIDENCIA. TECLEE 1 O 2:')
+                print('1 - TOTAL') #AND
+                print('2 - PARCIAL') #OR
+                coincidence = input() 
+            print('INGRESE LA CONSULTA DESEADA')
+            query = input()
 
-        content = listdir(path)
+            content = LoadFile(path)
 
-        result = ExcecuteModel(content, query, queryMode, coincidence)
+            result = ExcecuteModel(content, query, queryMode, coincidence)
 
-        print('\nRESULTADOS DE LA CONSULTA: \n')
-        if(len(result) == 0):
-            print('NO SE ENCONTRARON COINCIDENCIAS')
-        for i, doc in enumerate(result):
-            print(i, '-', doc)
-        print('\n')
+            print('\nRESULTADOS DE LA CONSULTA: \n')
+            if(len(result) == 0):
+                print('NO SE ENCONTRARON COINCIDENCIAS')
+            for i, doc in enumerate(result):
+                print(i, '-', doc)
+            print('\n')
+            flag = False
         
+        if(flag):
+            print('El path es invalido')
         print('TECLEE: ')
         print('1 - Salir')
         print('2 - Hacer otra consulta')
