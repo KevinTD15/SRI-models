5bd63bd175dec8ecf86578a7ef8885b6baf12e14
KevinTD15 <talaveradiazkevin1999@gmail.com>
2022-11-26 11:58:21 -0500

    modificado para poder realizar consultas de CRANFIELD

--- main.py
+++ main.py
@@ -23,9 +23,23 @@ def main():
         cran = input()
         if(cran == '1'):
             content = []
+            cranQuery = []
             dat = ir_datasets.load('cranfield')
             for doc in dat.docs_iter():
                 content.append([doc.author + ' ' + doc.title, doc.author + ' ' + doc.title + ' ' + doc.text])
+            for q in dat.queries_iter():
+                cranQuery.append(q.text)
+            
+            print('DESEA USAR CONSULTAS DE CRANFIELD')
+            print('1- Si')
+            print('2- No')
+            cQuery = input()
+            
+            if(cQuery == '1'):
+                print('CUANTAS CONSULTAS REALIZAR. TECLEE UN RANGO DE NUMEROS')
+                cantIni = input()
+                cantEnd = input()
+            
         else:
             print('INGRESE EL PATH DONDE DESEA REALIZAR LA BUSQUEDA')
             path = input()
@@ -34,41 +48,54 @@ def main():
         if cran == '1' or os.path.exists(path):
             
             if(mod == '1'):
-                print('MODO DE CONSULTA. TECLEE 1 O 2:')
-                print('1 - Casual')
-                print('2 - Experto')
-                queryMode = input()
+                multResults = []    
+                if(cQuery == '2'):
+                    print('MODO DE CONSULTA. TECLEE 1 O 2:')
+                    print('1 - Casual')
+                    print('2 - Experto')
+                    queryMode = input()
 
-                coincidence = '0'
-                if(queryMode == '1'):
-                    print('TIPO DE COINCIDENCIA. TECLEE 1 O 2:')
-                    print('1 - TOTAL') #AND
-                    print('2 - PARCIAL') #OR
-                    coincidence = input()
-                    
-                print('INGRESE LA CONSULTA DESEADA')
-                query = input()
-                                       
-                result = ExcecuteModel(content, query, queryMode, coincidence)
+                    coincidence = '0'
+                    if(queryMode == '1'):
+                        print('TIPO DE COINCIDENCIA. TECLEE 1 O 2:')
+                        print('1 - TOTAL') #AND
+                        print('2 - PARCIAL') #OR
+                        coincidence = input()
 
-                print('\nRESULTADOS DE LA CONSULTA: \n')
-                if(len(result) == 0):
-                    print('NO SE ENCONTRARON COINCIDENCIAS')
-                for i, doc in enumerate(result):
-                    print(i, '-', doc)
-                print('\n')
+                    print('INGRESE LA CONSULTA DESEADA')
+                    query = input()
+                    multResults.append(ExcecuteModel(content, query, queryMode, coincidence))
+                else:
+                    query = cranQuery[int(cantIni):int(cantEnd)]
+                    for q in query:
+                        multResults.append(ExcecuteModel(content, q, queryMode, coincidence))
+                 
+                for j in range(len(multResults)):
+                    print(f'\nRESULTADOS DE LA CONSULTA: {query[j]}\n')
+                    if(len(multResults[j]) == 0):
+                        print('NO SE ENCONTRARON COINCIDENCIAS')
+                    for i, doc in enumerate(multResults[j]):
+                        if(doc[1] != 0):
+                            print(i, '-', doc,  '\n') 
                 
             elif(mod == '2'):
-                print('INGRESE LA CONSULTA DESEADA')
-                query = input()
+                multResults = []
+                if(cQuery == '2'):
+                    print('INGRESE LA CONSULTA DESEADA')
+                    query = input()
+                    multResults.append(ExcecuteModelV(content, query))
+                else:                   
+                    query = cranQuery[int(cantIni):int(cantEnd)]
+                    for q in query:
+                        multResults.append(ExcecuteModelV(content, q))                
                 
-                result = ExcecuteModelV(content, query)
-                print('\nRESULTADOS DE LA CONSULTA: \n')
-                if(len(result) == 0):
-                    print('NO SE ENCONTRARON COINCIDENCIAS')
-                for i in result:
-                    if(i[1] != 0):
-                        print(f'{i[0]} y su relevancia {i[1]} \n')
+                for j in range(len(multResults)):
+                    print(f'\nRESULTADOS DE LA CONSULTA: {query[j]}\n')
+                    if(len(multResults[j]) == 0):
+                        print('NO SE ENCONTRARON COINCIDENCIAS')
+                    for i in multResults[j]:
+                        if(i[1] != 0):
+                            print(f'Relevancia: {i[1]} --- Articulo: {i[0]}  \n')
             
             flag = False
         
