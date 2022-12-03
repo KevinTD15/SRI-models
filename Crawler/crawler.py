088645bb756d0c78f95d28718611b44635df9a4f
KevinTD15 <talaveradiazkevin1999@gmail.com>
2022-12-02 21:16:01 -0500

    completado el crawler

--- Crawler/crawler.py
+++ Crawler/crawler.py
@@ -2,6 +2,8 @@ import queue
 import requests
 from bs4 import BeautifulSoup
 import re
+import time
+import os
 
 def GetLinks(url):
     return re.findall('"((http)s?://.*?)"', url)
@@ -19,17 +21,45 @@ def GetTextFromHtml(text):
     commands = [('script', 9), ('span', 7), ('style', 8)]
     for i in commands:
         text = DeleteCommand(text, i)
-    soap = BeautifulSoup(text, 'lxml')
-    return soap.get_text()
+    soap = BeautifulSoup(text, features='lxml').get_text()
+    soap = soap.replace('\n', '')
+    return soap
 
-def Crawl(seedUrl, anchor):
-    urls = []
-    anchorQueue = queue.Queue()
-    while True:
-        response = requests.get(seedUrl + anchor)
+def IsTrueWeb(url):
+    url = url[0]
+    return (url.count('.ico') + url.count('.gif') + url.count('.jpg') + url.count('.js') + url.count('.png') + url.count('.css')) == 0
+
+def createNewTxt(folder):    
+    return os.path.join(folder, str(len(os.listdir(folder))) + ".txt")
+
+def saveDoc(url,text):
+    
+    with open(createNewTxt('Crawler\cache'),'w', encoding='utf8', errors='ignore') as f:
+        f.write(url[0] + '\n' + text)
+
+def Crawl(seedUrl, t):
+    visitedUrl = set()
+    parsedUrl = []
+    htmlText = []
+    start = time.time()
+    end = time.time()
+    while len(seedUrl)>0 and (end-start)<=t:
+        visitedUrl.add(seedUrl[0])
+        url = seedUrl.pop(0)
+        parsedUrl.append(url)
+        response = requests.get(url[0])
         links = GetLinks(response.text)
-        htmlText = GetTextFromHtml(response.text)
+        htmlText.append(GetTextFromHtml(response.text))
+        direct = os.listdir('Crawler\cache')
+        for i in direct:
+            a = open('Crawler/cache/'+i,encoding='utf8', errors='ignore')
+            if(url[0] in a.read()):
+                break
+        saveDoc(url, htmlText[len(htmlText) - 1])
         if(len(links) > 0):
             for link in links:
-                return                
-        a = 5
+                if(IsTrueWeb(link) and link not in seedUrl and link not in visitedUrl):
+                    visitedUrl.add(link)
+                    seedUrl.append(link) 
+        end = time.time()   
+    return htmlText, parsedUrl
