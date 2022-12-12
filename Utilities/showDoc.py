f4a5f64fa138b67bd19547deecd8a200ab19901c
KevinTD15 <talaveradiazkevin1999@gmail.com>
2022-12-12 02:07:08 -0500

    creado py para abrir documentos

---
+++ Utilities/showDoc.py
@@ -0,0 +1,26 @@
+from tkinter import *
+
+def Content(path, texto):
+    
+        vdoc = Tk()
+        vdoc.title(path) 
+        vdoc.geometry("610x500"+"+"+str(540)+"+"+str(10)) 
+        vdoc.resizable(0,0)
+        vdoc.config(bg="blue", bd=12, relief="sunken")
+      
+        hscrollbar = Scrollbar(vdoc, orient=HORIZONTAL)
+        vscrollbar = Scrollbar(vdoc, orient=VERTICAL)
+
+        resultList=  Listbox(vdoc, height=27, width=81, bg="black",fg="white",font=("arial",10),
+                                 xscrollcommand=hscrollbar.set, yscrollcommand=vscrollbar.set)
+        
+        hscrollbar.config(command=resultList.xview)
+        hscrollbar.pack(side=BOTTOM, fill=X)
+        vscrollbar.config(command=resultList.yview)
+        vscrollbar.pack(side=RIGHT, fill=Y)
+              
+        resultList.place(x=0,y=0)        
+        resultList.config(xscrollcommand=hscrollbar.set, yscrollcommand=vscrollbar.set)
+              
+        resultList.insert(END ,texto)
+        vdoc.mainloop()
