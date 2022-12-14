2f8ed64423bbe67f1ec0650637477ed7b0c0f4b9
KevinTD15 <talaveradiazkevin1999@gmail.com>
2022-12-14 01:16:40 -0500

    arreglado muestreo de documentos

--- Utilities/showDoc.py
+++ Utilities/showDoc.py
@@ -8,7 +8,7 @@ def Content(path, texto):
         vdoc.resizable(0,0)
         vdoc.config(bg="blue", bd=12, relief="sunken")
       
-        hscrollbar = Scrollbar(vdoc, orient=HORIZONTAL)
+        '''hscrollbar = Scrollbar(vdoc, orient=HORIZONTAL)
         vscrollbar = Scrollbar(vdoc, orient=VERTICAL)
 
         resultList=  Listbox(vdoc, height=27, width=81, bg="black",fg="white",font=("arial",10),
@@ -22,5 +22,10 @@ def Content(path, texto):
         resultList.place(x=0,y=0)        
         resultList.config(xscrollcommand=hscrollbar.set, yscrollcommand=vscrollbar.set)
               
-        resultList.insert(END ,texto)
+        resultList.insert(END ,texto)'''
+        cuadroTexto = Text (vdoc)
+        cuadroTexto.pack(fill="both",expand=1)
+        cuadroTexto.config(bd=0,padx=0)
+        cuadroTexto.delete(1.0,'end')
+        cuadroTexto.insert('insert',texto)
         vdoc.mainloop()
