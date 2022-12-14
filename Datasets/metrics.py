351ef3d9f6f6e3f5f2763b7ab1ea348308ed2641
KevinTD15 <talaveradiazkevin1999@gmail.com>
2022-12-14 11:42:59 -0500

    arreglada division por 0

--- Datasets/metrics.py
+++ Datasets/metrics.py
@@ -5,14 +5,14 @@ def Evaluate(solution, qrels):
     rel = set(qrels)
     rr = rel.intersection(rec)
     ri = rec.difference(rr)
-    accuracy = len(rr) / len(rr.union(ri))
+    accuracy = len(rr) / len(rr.union(ri)) if len(rr.union(ri)) > 0 else 0
     nr = rel.difference(rr)
-    recovered = len(rr) / len(rr.union(nr))
+    recovered = len(rr) / len(rr.union(nr)) if len(rr.union(nr)) > 0 else 0
     return accuracy, recovered
 
 def F1(accuracy, recovered):
-    return (2 * accuracy * recovered) / (accuracy + recovered)
+    return (2 * accuracy * recovered) / (accuracy + recovered) if accuracy + recovered > 0 else 0
 
 def F(accuracy, recovered, beta = 0):
-    return ((1 + beta**2) * accuracy * recovered) / (beta**2 * accuracy + recovered)
+    return ((1 + beta**2) * accuracy * recovered) / (beta**2 * accuracy + recovered) if beta**2 * accuracy + recovered > 0 else 0
     
